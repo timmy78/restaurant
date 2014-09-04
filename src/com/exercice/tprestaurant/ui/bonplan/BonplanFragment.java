80e000cc58c7436a5dcb7558313871842f2f65ce
timothee <timothee.planchais@gmail.com>
2014-09-04 15:48:26 +0200

    Fragment bon plan

--- src/com/exercice/tprestaurant/ui/bonplan/BonplanFragment.java
+++ src/com/exercice/tprestaurant/ui/bonplan/BonplanFragment.java
@@ -1,17 +1,29 @@
 package com.exercice.tprestaurant.ui.bonplan;
 
+import utils.Preferences;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.exercice.tprestaurant.R;
 import com.exercice.tprestaurant.backend.data.Commercant;
 
 public class BonplanFragment extends Fragment {
 	
+	private Activity mActivity;
 	private Commercant cm;
+	private TextView contenu;
+	
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		this.mActivity = activity;
+		super.onAttach(activity);
+	}
 	
 	public BonplanFragment() {
 		// Required empty public constructor
@@ -37,4 +49,15 @@ public class BonplanFragment extends Fragment {
 		super.onViewCreated(view, savedInstanceState);
 	}
 	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		
+		this.cm = Preferences.getInformations(mActivity);
+		this.contenu = (TextView) getActivity().findViewById(R.id.bp_text);
+		
+		this.contenu.setText(cm.getBonplanContenu());
+		
+		super.onActivityCreated(savedInstanceState);
+	}
 }
